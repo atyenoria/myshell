ae918ed0d8e370f821e664ee280c486b97dd1f3b
atyenoria <eigotyoubunn30@gmail.com>
2015-06-05 09:55:50 +0900

    docker_process_kill

--- pm2/host.sh
+++ pm2/host.sh
@@ -2,6 +2,6 @@ F=/Users/jima/work/rsyc_vagrant/test/host
 S=/home/vagrant/host
 
 fswatch -0 /Users/jima/work/rsyc_vagrant/test/host | while read -d "" event 
-do 
+/do 
     rsync --progress -avz $F/ vagrant@192.168.200.200:$S
 done
